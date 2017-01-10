024560710c9c86f6bff3ac20e5ad6f125d7d777d
Adam Green <adamgr@foo.bar>
2017-01-09 23:57:26 -0800

    Add UART interface for setting frequency & amplitude
    Can type decimal digits followed by return/enter to update frequency.
    Can press + or - to update the amplitude as percentage of maximum.

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -12,69 +12,105 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+#include <ctype.h>
 #include <mbed.h>
 #include "FrequencyGenerator.h"
 
-// Function Prototypes.
 
-//#define FREQUENCY
+static Serial            g_serial(USBTX, USBRX);
+static volatile uint32_t g_frequency = 1000;
+static volatile uint32_t g_amplitude = 50;
+static volatile bool     g_charsEchoed = false;
+
+
+// Function Prototypes.
+static void serialRxHandler(void);
+
 
 int main()
 {
     static   DigitalOut         myled(LED1);
     static   Timer              ledTimer;
     static   FrequencyGenerator freqGen(p18);
-    uint32_t amplitude = 100;
-    uint32_t frequency = 1000;
-    bool     isDecreasing = true;
+    uint32_t                    lastFrequency = 0;
+    uint32_t                    lastAmplitude = 0;
 
+    g_serial.baud(230400);
+    g_serial.attach(serialRxHandler);
+
+    freqGen.setAmplitude(g_amplitude);
     freqGen.start();
 
     ledTimer.start();
     while(1)
     {
+        uint32_t currFrequency = g_frequency;
+        uint32_t currAmplitude = g_amplitude;
+
+        if (currFrequency != lastFrequency)
+        {
+            freqGen.setFrequency(currFrequency);
+            printf("%sFrequency=%lu\r\n", g_charsEchoed ? "\r\n" : "", currFrequency);
+            lastFrequency = currFrequency;
+            g_charsEchoed = false;
+        }
+
+        if (currAmplitude != lastAmplitude)
+        {
+            freqGen.setAmplitude(currAmplitude);
+            printf("%sAmplitude=%lu%%\r\n", g_charsEchoed ? "\r\n" : "", currAmplitude);
+            lastAmplitude = currAmplitude;
+            g_charsEchoed = false;
+        }
+
         if (ledTimer.read_ms() >= 250)
         {
             myled = !myled;
             ledTimer.reset();
+        }
+    }
+}
 
-            if (isDecreasing)
+static void serialRxHandler(void)
+{
+    static uint32_t frequency = 0;
+
+    while (g_serial.readable())
+    {
+        char curr = g_serial.getc();
+
+        if (isdigit(curr))
+        {
+            g_serial.putc(curr);
+            g_charsEchoed = true;
+
+            frequency = frequency * 10 + (curr - '0');
+        }
+        else if (curr == '\n')
+        {
+            if (frequency > 1000)
+                frequency = 1000;
+
+            g_serial.putc('\r');
+            g_serial.putc('\n');
+            g_charsEchoed = false;
+
+            g_frequency = frequency;
+            frequency = 0;
+        }
+        else if (curr == '+')
+        {
+            if (g_amplitude < 100)
             {
-#ifdef FREQUENCY
-                frequency-=50;
-                if (frequency == 50)
-                {
-                    isDecreasing = false;
-                }
-#else
-                amplitude--;
-                if (amplitude == 0)
-                {
-                    isDecreasing = false;
-                }
-#endif
+                g_amplitude++;
             }
-            else
+        }
+        else if (curr == '-')
+        {
+            if (g_amplitude > 0)
             {
-#ifdef FREQUENCY
-                frequency+=50;
-                if (frequency == 1000)
-                {
-                    isDecreasing = true;
-                }
-#else
-                amplitude++;
-                if (amplitude == 100)
-                {
-                    isDecreasing = true;
-                }
-#endif
+                g_amplitude--;
             }
-#ifdef FREQUENCY
-            freqGen.setFrequency(frequency);
-#else
-            freqGen.setAmplitude(amplitude);
-#endif
         }
     }
 }
